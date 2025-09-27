d4acf7896010973ee9b7d5a1902ca042a48b499c
AnkoX22 <ankoxhemaj@gmail.com>
2025-09-28 01:39:24 +0300

    create pearson correlation functions

--- src/utils.py
+++ src/utils.py
@@ -13,7 +13,7 @@ def manual_cosine_similarity(X, Y):
     x_squared = 0
     y_squared = 0
 
-    for i in range(X.shape[0]):
+    for i in range(len(X)):
         dot_product += X[i] * Y[i]
         x_squared += X[i] * X[i]
         y_squared += Y[i] * Y[i]
@@ -24,3 +24,44 @@ def manual_cosine_similarity(X, Y):
         return 0
 
     return dot_product / denominator
+
+def np_pearson_correlation(X, Y):
+    X_cebtered = X - X.mean()
+    Y_cebtered = Y - Y.mean()
+
+    numerator = np.sum(X_cebtered * Y_cebtered)
+    denominator = np.sqrt(np.sum(X_cebtered**2))*np.sqrt(np.sum(Y_cebtered**2))
+
+    if denominator == 0:
+        return 0
+    return numerator / denominator
+
+def manual_pearson_correlation(X, Y):
+
+    X = np.array(X)
+    Y = np.array(Y)
+
+    if(len(X) != len(Y)):
+        raise ValueError("Vectors must be the same length")
+
+    if len(X) == 0:
+        return 0
+
+    X_total = 0
+    Y_total = 0
+
+    for i in range(len(X)):
+        X_total += X[i]
+        Y_total += Y[i]
+
+    X_average = X_total / len(X)
+    Y_average = Y_total / len(Y)
+
+    X_centered = X - X_average
+    Y_centered = Y - Y_average
+
+    numerator = np.sum(X_centered * Y_centered)
+    denominator = np.sqrt(np.sum(X_centered**2))*np.sqrt(np.sum(Y_centered**2))
+
+    return numerator / denominator
+
